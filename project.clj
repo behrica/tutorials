ad91a8aa67d672d54db7e3cf7e5b6b0ff6e5adaa
Carsten Behring <carsten.behring@gmail.com>
2019-03-03 12:04:25 +0100

    first version of rotten tomatoes tutorail

--- project.clj
+++ project.clj
@@ -4,4 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [alembic "0.3.2"]])
+                 ]
+
+:plugins [[lein-jupyter "0.1.16"]]
+  )
